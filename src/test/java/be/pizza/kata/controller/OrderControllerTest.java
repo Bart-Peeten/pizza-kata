49b01eaf85a1c63c58a5a9ee2f2d50a589be6cc2
bartpeeten <bart.peeten@gmail.com>
2025-05-23 16:41:15 +0200

    Add unit test for OrderController's placeOrder method
    Introduced a test to verify the correctness of the placeOrder method in OrderController. This ensures that the order creation and delivery time estimation logic work as expected.

---
+++ src/test/java/be/pizza/kata/controller/OrderControllerTest.java
@@ -0,0 +1,52 @@
+package be.pizza.kata.controller;
+
+import be.pizza.kata.enitity.PizzaOrder;
+import be.pizza.kata.request.OrderRequest;
+import be.pizza.kata.response.OrderResponse;
+import be.pizza.kata.service.PizzaOrderService;
+import be.pizza.kata.service.impl.DefaultDeliveryTimeEstimatorService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OrderControllerTest {
+
+    public static final UUID uuid = UUID.randomUUID();
+
+    private OrderRequest orderRequest;
+
+    @Mock
+    PizzaOrder pizzaOrder;
+    @Mock
+    private PizzaOrderService pizzaOrderService;
+    @Spy
+    private DefaultDeliveryTimeEstimatorService deliveryTimeEstimatorService;
+
+    @InjectMocks
+    private OrderController orderController;
+
+    @Test
+    void testPlaceOrder() {
+        orderRequest = new OrderRequest();
+        orderRequest.setPizza("MARGHERITA");
+        orderRequest.setSize("MEDIUM");
+
+        when(pizzaOrderService.createOrder(orderRequest.getPizza(), orderRequest.getSize())).thenReturn(pizzaOrder);
+        when(pizzaOrder.getId()).thenReturn(uuid);
+        when(deliveryTimeEstimatorService.estimateDeliveryTime()).thenCallRealMethod();
+
+        final OrderResponse result = orderController.placeOrder(orderRequest);
+
+        assertEquals("20 minutes", result.getEstimatedTime());
+    }
+
+}
