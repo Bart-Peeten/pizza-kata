158159a634a5fb757604e0cf95c198ad933775b9
bartpeeten <bart.peeten@gmail.com>
2025-05-23 14:23:41 +0200

    Refactor OrderController to use new services for orders.
    Replaced direct repository usage with PizzaOrderService for creating orders and DeliveryTimeEstimatorService for estimating delivery times. This improves separation of concerns and simplifies future enhancements or testing. Updated constructor to initialize the new services.

--- src/main/java/be/pizza/kata/OrderController.java
+++ src/main/java/be/pizza/kata/OrderController.java
@@ -1,19 +1,29 @@
 package be.pizza.kata;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
+import be.pizza.kata.service.DeliveryTimeEstimatorService;
+import be.pizza.kata.service.PizzaOrderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class OrderController {
 
-    private final PizzaOrderRepository repository;
+    private final DeliveryTimeEstimatorService deliveryTimeEstimatorService;
+    private final PizzaOrderService pizzaOrderService;
 
-    @Autowired
-    public OrderController(PizzaOrderRepository repository) {
-        this.repository = repository;
+    /**
+     * Ctor to create a new instance of {@link OrderController}.
+     *
+     * @param deliveryTimeEstimatorService {@link DeliveryTimeEstimatorService} to use for estimating delivery time.
+     * @param pizzaOrderService            {@link PizzaOrderService} to use for creating new orders.
+     */
+    public OrderController(final DeliveryTimeEstimatorService deliveryTimeEstimatorService, final PizzaOrderService pizzaOrderService) {
+        this.deliveryTimeEstimatorService = deliveryTimeEstimatorService;
+        this.pizzaOrderService = pizzaOrderService;
     }
 
     @PostMapping("/order")
@@ -21,12 +31,8 @@ public class OrderController {
         String pizza = request.get("pizza");
         String size = request.get("size");
 
-        PizzaOrder order = new PizzaOrder();
-        order.setPizza(pizza);
-        order.setSize(size);
-        order = repository.save(order);
-
-        String estimatedTime = "20 minutes";
+        PizzaOrder order = pizzaOrderService.createOrder(pizza, size);
+        String estimatedTime = deliveryTimeEstimatorService.estimateDeliveryTime();
 
         Map<String, String> response = new HashMap<>();
         response.put("orderId", order.getId().toString());
